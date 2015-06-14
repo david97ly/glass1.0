5e04a496d35b00816c6dedfc22553ddd118b1630
David Cortez <david97ly@hotmail.com>
2015-06-13 23:31:10 -0600

    cambiando el defoutl

--- glass/models.py
+++ glass/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Orden(models.Model):
-	orden = models.CharField(max_length = 10)
+	orden = models.CharField(max_length = 10,default="Primero")
 
 class Fotos(models.Model):
 	nombre = models.CharField(max_length=500)
